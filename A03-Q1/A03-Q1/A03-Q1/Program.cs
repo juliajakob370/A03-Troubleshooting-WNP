6da3c3624b83e6f979b1298dc92a0d560345c30a
Bibi Murwarid Enayat Zada <morwarid.enayatzada063@gmail.com>
2026-03-17 09:08:35 -0400

    initial commit
    - Added files and folder that expected us to enhance and submit

---
+++ A03-Q1/A03-Q1/A03-Q1/Program.cs
@@ -0,0 +1,75 @@
+﻿// 
+// Place your file header comments here
+//
+using System.Diagnostics;
+
+namespace A03_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InitialCode();
+            BetterCode();
+
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
+        }
+
+        static void InitialCode()
+        {
+            // You may add code in this method for purposes of 
+            // measuring performance.
+            int temp = 0;
+            Random rand = new Random();
+            int LOOPCOUNT = 10000;
+            //
+            for (int counter = 0; counter < LOOPCOUNT; counter++)
+            {
+                float randomFloat = rand.NextSingle();
+                if (randomFloat < .10)
+                {
+                    temp = 1;
+                }
+                if (randomFloat >= .10 && randomFloat < .30)
+                {
+                    temp = 2;
+                }
+                if (randomFloat >= .3)
+                {
+                    temp = 3;
+                }
+            //
+            }
+        }
+
+        static void BetterCode()
+        {
+            // Rewrite the code from initial code here, with
+            // optimizations for time.
+            // You may add code in this method for purposes of 
+            // measuring performance.
+            int temp = 0;
+            Random rand = new Random();
+            int LOOPCOUNT = 10000;
+            //
+            for (int counter = 0; counter < LOOPCOUNT; counter++)
+            {
+                float randomFloat = rand.NextSingle();
+                if (randomFloat < .10)
+                {
+                    temp = 1;
+                }
+                if (randomFloat >= .10 && randomFloat < .30)
+                {
+                    temp = 2;
+                }
+                if (randomFloat >= .3)
+                {
+                    temp = 3;
+                }
+                //
+            }
+        }
+    }
+}
